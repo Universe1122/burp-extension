4740dea06fb020bb026a3631c0a0d03ec09e2ea3
Universe1122 <whdals7979@gmail.com>
2022-12-18 23:59:14 +0900

    초기 add comment popup UI

--- example/README.md
+++ example/README.md
@@ -1,3 +1,4 @@
 ## useful link
 - How to use Jframe all in one: https://wiki.python.org/jython/SwingExamples
 - jython docs: https://jythonbook-ko.readthedocs.io/en/latest/GUIApplications.html
+- gridbaglayout: https://kamang-it.tistory.com/entry/Swing-04Layout%EB%B0%B0%EC%B9%98%EA%B4%80%EB%A6%AC%EC%9E%90
--- frame/CommentUrlFrame.py
+++ frame/CommentUrlFrame.py
@@ -1,32 +1,93 @@
-from javax.swing import JButton, JFrame, JPanel, JLabel, JTextArea, JTextField
-from java.awt import Component, GridLayout, Frame
+from javax.swing import JButton, JFrame, JPanel, JLabel, JTextArea, JTextField, JPanel, BorderFactory
+from java.awt import Component, GridLayout, Frame, GridBagLayout, GridBagConstraints, Dimension, Insets
 
 
 class CommentUrlFrame():
 
     def __init__(self, window_closing_handler):
         self.frame = JFrame("Add comments about this url",
-            size = (300, 300)
+            size = (600, 300)
         )
 
         self.frame.setLocationRelativeTo(self.getBurpFrame())
         self.windowClosing = window_closing_handler
 
-        panel = JPanel(GridLayout(2,2))
-        self.frame.add(panel)
+        panel = JPanel()
+        gbl = GridBagLayout()
+        panel.setLayout(gbl)
+        panel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5))   # padding
 
-        panel.add(JLabel("Select URL: "))
-        panel.add(JTextField(""))
+        ################################
+        # -> Create URL Info Layout
+        #   -> Create Text Label
+        gbc = GridBagConstraints()
+        gbc.insets = Insets(0,0,5,5)    # padding
+        gbc.gridx = 0
+        gbc.gridy = 0
+        gbc.weightx = 0.1
+        gbc.fill = GridBagConstraints.BOTH
+        panel.add(JLabel(" Select URL "), gbc)
 
+        #   -> Create URL Text Field
+        gbc = GridBagConstraints()
+        gbc.insets = Insets(0,0,5,5)    # padding
+        gbc.gridx = 1
+        gbc.gridy = 0
+        gbc.weightx = 0.9
+        gbc.fill = GridBagConstraints.BOTH
+        panel.add(JTextField(20), gbc)
+        # -> End
+        ################################
+
+        ################################
+        # -> Create Comment Info Layout
+        #   -> Create Comment Label
+        gbc = GridBagConstraints()
+        gbc.gridx = 0
+        gbc.gridy = 2
+        gbc.fill = GridBagConstraints.BOTH
+        panel.add(JLabel(" Enter Comments "), gbc)
+
+        #   -> Create Comment TextArea Field
+        gbc = GridBagConstraints()
+        gbc.gridx = 1
+        gbc.gridy = 2
+        # gbc.gridwidth = 2
+        # gbc.gridheight = 2
+        gbc.weighty = 0.7
+        gbc.fill = GridBagConstraints.BOTH
         panel.add(JTextArea(
             text = "TextArea",
             editable = True,
             wrapStyleWord = True,
             lineWrap = True,
             alignmentX = Component.LEFT_ALIGNMENT,
-            size = (300, 1)
-        ))
+            size = (300, 40)
+        ), gbc)
+        # -> End
+        ################################
 
+
+        ################################
+        # -> Create Button
+        #   -> Create Cancel Button
+        gbc = GridBagConstraints()
+        gbc.gridx = 1
+        gbc.gridy = 4
+        gbc.insets = Insets(5,5,0,0)
+        gbc.anchor = GridBagConstraints.WEST
+        panel.add(JButton(" Cancel "), gbc)
+
+        gbc = GridBagConstraints()
+        gbc.gridx = 1
+        gbc.gridy = 4
+        gbc.insets = Insets(5,5,0,0)
+        gbc.anchor = GridBagConstraints.EAST
+        panel.add(JButton(" OK "), gbc)
+        # -> End
+        ################################
+
+        self.frame.setContentPane(panel)
         self.frame.show()
     
     def getBurpFrame(self):
